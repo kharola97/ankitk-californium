90ae9b8ee262121e6ab54f244d640ef0275b3ee7
kharola97 <akharola4@gmail.com>
2022-12-24 00:10:51 +0530

    ipurl

---
+++ src/middlewares/reusemiddle.js
@@ -0,0 +1,19 @@
+
+
+
+const middlewareapp= function (req, res, next) {
+    var currentdate = new Date(); 
+    var datetime =  currentdate.getDate() + " "
+                    + (currentdate.getMonth()+1)  + " " 
+                    + currentdate.getFullYear() + "  "  
+                    + currentdate.getHours() + ":"  
+                    + currentdate.getMinutes() + ":" 
+                    + currentdate.getSeconds();
+
+    let ip= req.ip
+    let url= req.originalUrl
+    console.log(`${datetime}  ${ip}  ${url}`)
+    next()    
+}
+
+module.exports = {middlewareapp}
