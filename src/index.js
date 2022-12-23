085ffb8b06391bc71765bdb8b120ad3e936e8de4
kharola97 <akharola4@gmail.com>
2022-12-24 00:12:47 +0530

    index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const {middlewareapp} = require('./middlewares/reusemiddle')
 const { default: mongoose } = require('mongoose');
 const app = express();
 
@@ -8,19 +9,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+app.use(middlewareapp)
+
+
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
 app.use('/', route);
 // unreachable becuase the cycle has terminated
 app.use(
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        console.log("inside GLOBAL MW");
         next();
-  }
+    }
 );
 
 
