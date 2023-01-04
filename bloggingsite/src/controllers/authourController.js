973977782e5a8f46271d08fc77fa807c2acfabdb
nirmalasharma123 <119753743+nirmalasharma123@users.noreply.github.com>
2023-01-04 17:25:12 +0530

    Update authourController.js

--- bloggingsite/src/controllers/authourController.js
+++ bloggingsite/src/controllers/authourController.js
@@ -1,6 +1,6 @@
-
 const validator=require("validator")
 const author=require("../modules/authorModel");
+const jwt=require("jsonwebtoken")
 const createAuthor=async function(req,res){
   try {
     let data=req.body;
@@ -16,17 +16,51 @@ const createAuthor=async function(req,res){
      if(!(validator.isEmail(req.body.email))) return res.status(400).send("please put a valid email")
      
      if (!validator.isStrongPassword(data.password)) {
-      return res.status(400).send({ status: false, msg: "Kindly use atleast one uppercase alphabets, numbers and special characters for strong password." })
+      return res.status(400).send({ status: false, msg: "Kindly use atleast one uppercase alphabets, numbers and special characters for strong password."})
 }
      
      
      let setData=await author.create(data);
-      data.email
     res.status(201).send({status:true,msg:setData});
 }catch(error){
     res.status(500).send({status:false,error:error.message})
 }
     
 
+};
+
+
+//========================================================================
+
+
+
+const login = async(req, res) => {
+  try {
+      let username = req.body.email
+      let password = req.body.password
+
+      if (!username || !password) {
+          return res.status(400).send({ status: false, msg: "Please Enter email id and password both." })
+      }
+
+      let author1 = await author.findOne({ email: username }).select({ email: 1, password: 1 })
+      if (!author1) {
+          return res.status(400).send({ status: false, msg: "Please enter correct email." })
+      }
+      if (password !== author1.password) {
+          return res.status(401).send({ status: false, msg: "Email Id and password are not matched. Enter the correct password." })
+      }
+
+      let token = jwt.sign({ authorId: author1._id.toString()},"californium-blog")
+      
+      res.setHeader("x-api-key", token)
+      res.status(201).send({ status: true, data: token })
+
+  } catch (error) {
+      res.status(500).send({ status: false, msg: error.message })
+  }
 }
-module.exports={createAuthor};
+
+
+module.exports={createAuthor,login};
+  
