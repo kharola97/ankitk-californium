3c1a5a97e9cfb2bc427078486cba586a45f934d7
nirmalasharma123 <119753743+nirmalasharma123@users.noreply.github.com>
2023-01-04 17:27:57 +0530

    Update route.js

--- bloggingsite/src/route/route.js
+++ bloggingsite/src/route/route.js
@@ -2,12 +2,23 @@ const express=require("express");
 const router=express.Router();
 const authorController=require("../controllers/authourController");
 const blogController=require('../controllers/bloggController')
+const middleWare=require("../middleware/middl")
 
 router.post("/authors",authorController.createAuthor);
-router.post('/blogs',blogController.createBlog);
+//=========================================================
+router.post('/blogs',middleWare.authen, blogController.createBlog);
+//====================================================================
 router.get("/blogs",blogController.getBlogs);
-router.put("/blogs/:blogId",blogController.updateBlog);
-router.delete("/blogs/:blogId",blogController.deletById)
+//========================================================================
+router.put("/blogs/:blogId",middleWare.authen,middleWare.authoris,blogController.updateBlog);
+//====================================================================================================
+router.delete("/blogs/:blogId",middleWare.authen,middleWare.authoris,blogController.deletById);
+//======================================================================================================
+router.delete("/blogs",middleWare.authen,middleWare.authoris,blogController.deleteQuery);
+//==========================================================================================================
+
+router.post("/login",authorController.login);
+
 
 
 
