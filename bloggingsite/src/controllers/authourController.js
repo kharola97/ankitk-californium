3080bf60588e70c39ecac368fb4d80f83f1b4813
Abhinav Gupta <98967744+abhinav101901@users.noreply.github.com>
2023-01-05 13:30:54 +0530

    Update authourController.js

--- bloggingsite/src/controllers/authourController.js
+++ bloggingsite/src/controllers/authourController.js
@@ -1,38 +1,30 @@
-const validator=require("validator")
+const jwt = require("jsonwebtoken");
+const validator=require("validator");
 const author=require("../modules/authorModel");
-const jwt=require("jsonwebtoken")
+
 const createAuthor=async function(req,res){
   try {
     let data=req.body;
 
-    if(!(validator.isAlpha(data.fname)||validator.isAlpha(data.lname)))  return res.status(400).send("invalid name")
+    if(!(validator.isAlpha(data.fname)||validator.isAlpha(data.lname)))  return res.status(400).send({status:false,msg:"invalid name"})
      
     
     let fname=data.fname.trim().split(" ").join("");
      let lname=data.lname.trim().split(" ").join("");
-    data.fname=fname
+    data.fname=fname;
     data.lname=lname;
 
-     if(!(validator.isEmail(req.body.email))) return res.status(400).send("please put a valid email")
+     if(!(validator.isEmail(req.body.email))) return res.status(400).send({status:false,msg:"please put a valid email"})
      
      if (!validator.isStrongPassword(data.password)) {
-      return res.status(400).send({ status: false, msg: "Kindly use atleast one uppercase alphabets, numbers and special characters for strong password."})
+      return res.status(400).send({ status: false, msg: "Kindly use atleast one uppercase alphabets, numbers and special characters for strong password." })
 }
-     
-     
      let setData=await author.create(data);
-    res.status(201).send({status:true,msg:setData});
+    res.status(201).send({status:true,data:setData});
 }catch(error){
-    res.status(500).send({status:false,error:error.message})
+    res.status(500).send({status:false,msg:error.message})
 }
-    
-
-};
-
-
-//========================================================================
-
-
+}
 
 const login = async(req, res) => {
   try {
@@ -43,24 +35,23 @@ const login = async(req, res) => {
           return res.status(400).send({ status: false, msg: "Please Enter email id and password both." })
       }
 
-      let author1 = await author.findOne({ email: username }).select({ email: 1, password: 1 })
-      if (!author1) {
+      let auth = await author.findOne({ email: username }).select({ email: 1, password: 1 })
+
+      if (!auth) {
           return res.status(400).send({ status: false, msg: "Please enter correct email." })
       }
-      if (password !== author1.password) {
+      if (password !== auth.password) {
           return res.status(401).send({ status: false, msg: "Email Id and password are not matched. Enter the correct password." })
       }
 
-      let token = jwt.sign({ authorId: author1._id.toString()},"californium-blog")
-      
+      let token = jwt.sign({ authorId: auth._id.toString(), batch: "californium"},"californium-blog");
       res.setHeader("x-api-key", token)
       res.status(201).send({ status: true, data: token })
 
   } catch (error) {
-      res.status(500).send({ status: false, msg: error.message })
+      res.status(500).send({ status: false, msg: error.message })
   }
 }
 
 
 module.exports={createAuthor,login};
-  
